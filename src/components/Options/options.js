1917da36f01675653068dfe1d951fc0c3f280429
Curtis Timson <curt@live.co.uk>
2018-06-05 21:29:26 +0100

    Merge conflict

--- src/components/Options/options.js
+++ src/components/Options/options.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-<<<<<<< HEAD
 import './options.css';
 
 class Options extends Component {
@@ -14,15 +13,10 @@ class Options extends Component {
     console.log('Handle Option Choice', option);
   }
 
-=======
-
-class Options extends Component {
->>>>>>> master
   render(){
 
     let options = this.props.item.options.map(option => (
       <li key={option}>
-<<<<<<< HEAD
         <button onClick={(e) => this.handleOptionChoice(e, option)}>{option}</button>
       </li>
     ));
@@ -36,17 +30,6 @@ class Options extends Component {
             {options}
           </ul>
         </div>
-=======
-        {option}
-      </li>
-    ));
-
-    return (
-      <div className="Options">
-        <ul>
-          {options}
-        </ul>
->>>>>>> master
       </div>
     )
   }
