b08efbfb1423dd1a2cf6609060b128b4180beb71
Curtis Timson <curt@live.co.uk>
2018-06-05 19:07:47 +0100

    Move map configuration to new file

--- src/Map.js
+++ src/Map.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
+import MapConfig from './config/MapConfig';
 
 class Map extends Component {
   render() {
+
+    let mapUrl = `https://www.google.com/maps/embed/v1/place?key=${MapConfig.API_KEY}&q=Space+Needle,Seattle+WA`;
+
     return (
       <div className="Map">
         <iframe
           width="100%"
           height="450"
           frameborder="0"
-          src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBCiteXn1FyqNNcXdQi1xI_UCF7ntT3j2s&amp;q=Space+Needle,Seattle+WA" allowfullscreen>
+          src={mapUrl}
+          allowfullscreen>
         </iframe>
       </div>
     );
---
+++ src/config/MapConfig.js
@@ -0,0 +1,5 @@
+let MapConfig = {
+    API_KEY: 'AIzaSyBCiteXn1FyqNNcXdQi1xI_UCF7ntT3j2s'
+}
+
+export default MapConfig;
