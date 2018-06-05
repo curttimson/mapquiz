eb631367685a3f3a654e0a84e227f53475c73052
Curtis Timson <curt@live.co.uk>
2018-06-05 21:31:14 +0100

    Map data (#3)
    * Move map to components
    
    * Add options component
    
    * Show options in options component
    
    * Add key
    
    * Remove console log
    
    * Bump version
    
    * Handle option click
    
    * Merge conflict

--- src/components/Options/options.js
+++ src/components/Options/options.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react';
+import './options.css';
 
 class Options extends Component {
+
+  constructor(props) {
+    super();
+    this.handleOptionChoice = this.handleOptionChoice.bind(this);
+  }
+
+  handleOptionChoice(e, option){
+    e.preventDefault();
+    console.log('Handle Option Choice', option);
+  }
+
   render(){
 
     let options = this.props.item.options.map(option => (
       <li key={option}>
-        {option}
+        <button onClick={(e) => this.handleOptionChoice(e, option)}>{option}</button>
       </li>
     ));
 
+    
+
     return (
       <div className="Options">
-        <ul>
-          {options}
-        </ul>
+        <div className="inner">
+          <ul>
+            {options}
+          </ul>
+        </div>
       </div>
     )
   }
