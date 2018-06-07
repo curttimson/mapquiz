e4305f43cbeaf2d3f3b2f0be941e9a343854861a
Curtis Timson <curt@live.co.uk>
2018-06-07 08:41:59 +0100

    Feature/additional maps (#5)
    * Add more maps
    
    * Show alert when all rounds are complete

--- src/data/mapdata.js
+++ src/data/mapdata.js
@@ -10,6 +10,42 @@ let MapData = [
     long: '4.896127',
     options: ['Austria', 'Germany', 'Netherlands', 'Portugal'],
     correctOption: 'Netherlands'
+  },
+  {
+    lat: '30.108085',
+    long: '31.488073',
+    options: ['Germany', 'Egypt', 'Iran', 'Libya'],
+    correctOption: 'Egypt'
+  },
+  {
+    lat: '37.079390',
+    long: '139.739952',
+    options: ['Ireland', 'New Zealand', 'Japan', 'Taiwan'],
+    correctOption: 'Japan'
+  },
+  {
+    lat: '65.815211',
+    long: '17.703820',
+    options: ['Sweden', 'Finland', 'Norway', 'Netherlands'],
+    correctOption: 'Sweden'
+  },
+  {
+    lat: '50.476819',
+    long: '33.040734',
+    options: ['Romania', 'Ukraine', 'Russia', 'Belarus'],
+    correctOption: 'Ukraine'
+  },
+  {
+    lat: '32.255492',
+    long: '-116.427587',
+    options: ['Canada', 'Colombia', 'USA', 'Mexico'],
+    correctOption: 'Mexico'
+  },
+  {
+    lat: '-20.100993',
+    long: '46.141367',
+    options: ['Madagascar', 'Indonesia', 'Japan', 'Cuba'],
+    correctOption: 'Madagascar'
   }
 ];
 
