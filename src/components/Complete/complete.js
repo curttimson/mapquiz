447e36809413d0971200ae8a6ca1d5c2f5a1075e
Curtis Timson <curt@live.co.uk>
2018-06-26 13:35:19 +0100

    Add complete component and route

---
+++ src/components/Complete/complete.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Complete extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+  render() {
+    return (
+      <div className="Map">
+        Complete
+      </div>
+    );
+  }
+}
+
+export default Complete;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import './index.css';
 import App from './App';
+import Complete from './components/Complete/complete';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path='/' component={App}/>
+            <Route path='/complete' component={Complete}/>
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
 registerServiceWorker();
