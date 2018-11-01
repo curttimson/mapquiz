d3af58b43a707777e9c48698a21d321719545b05
Curtis Timson <curt@live.co.uk>
2018-11-01 16:41:22 +0000

    Add Map Display story

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import { storiesOf, action } from '@storybook/react';
+import MapDisplay from '../src/components/MapDisplay/map-display';
 import Options from '../src/components/Options/options';
+
+storiesOf('Map Display', module)
+    .add('UK', () => {
+        const mapItem = {
+            lat: 51.5074,
+            long: 0.1278,
+        };
+        return (<MapDisplay item={mapItem} />);
+    });
 
 storiesOf('Options', module)
     .add('Standard', () => (
